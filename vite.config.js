f0e6dd62460f66b8dc12d24729445258275e684e
Jan Kaiser <jan.kaiser@telefonica.com>
2025-10-30 10:17:26 +0100

    added base path to vite config

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/my-awesome-vue-app/",
   plugins: [
     vue(),
     vueDevTools(),
