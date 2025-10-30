d0c316cdd04aef951d6e4c37df0f77e96fda416e
Jan Kaiser <jan.kaiser@telefonica.com>
2025-10-30 10:09:10 +0100

    init

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<script>
+import MyButton from './components/MyButton.vue';
+export default {
+  components:{ MyButton },
+  data() {
+    return {
+      counter: 0,
+    }
+  },
+  methods: {
+    increase() { this.counter++ },
+    decrease() { this.counter-- },
+    reset() { this.counter = 0 },
+  }
+}
+
+</script>
+
+<template>
+  <div>
+    {{ counter }}
+  </div>
+  <MyButton text="-" :my-button-action="decrease"/>
+  <MyButton :text="2 * 5" :my-button-action="reset"/>
+  <MyButton text="+" @click="increase"/>
+</template>
+
+<style scoped>
+</style>
---
+++ src/components/MyButton.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+  props: {
+    text: Number,
+    myButtonAction: Function
+  },
+}
+
+</script>
+
+<template>
+  <button @mouseover="myButtonAction">{{  text }}</button>
+</template>
+
+<style scoped>
+</style>
